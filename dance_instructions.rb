01c124398c77aa0877aac6a89a1e4813005f1ae1
kwong991 <kgwong991@gmail.com>
2018-08-07 13:44:05 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,22 +2,23 @@ def starting stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
 end
 
-def Base_Footwork
+def base_footwork
   puts "Lift right foot"
   puts "Return right foot to the ground"
-  small_skip_back
-def skip_step
-  puts "Lower left foot to the ground"
-  puts "Bounce right foot back up slightly, kicking it a few inches back"
-  puts "Left, Right, Left and Left"
-def skip_step_right
-  puts "Lower right foot to the ground"
-  puts "Bounce right foot back up slightly, kicking it a few inches back"
-  puts "Right, Left, Right and Right"
+  skip_step("Left_Foot")
+end
+
+def skip_step(starting_foot)
+  puts "Lower #{starting_foot} foot to the ground"
+  puts "Bounce #{starting_foot} foot back up slightly, kicking it a few inches back"
+  puts "Briefly raise the opposite foot then the #{start_foot}"
+end
+
 def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."
+end
   bob_the_reins
   bob_the_reins
   bob_the_reins
@@ -30,7 +31,7 @@ def Lasso
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your elbow straight left"
   puts "Lift right arm up and point right elbow diagonally to the right"
   puts "whip it in small circles"
-  Lasso
+  lasso
   Lasso
   Lasso
   Lasso
