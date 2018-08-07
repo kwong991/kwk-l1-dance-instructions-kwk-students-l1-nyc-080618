12b2ad66c308b49c78f78cbec0e4a6a0ea54d537
kwong991 <kgwong991@gmail.com>
2018-08-07 13:59:58 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -11,7 +11,7 @@ end
 def skip_step(starting_foot)
   puts "Lower #{starting_foot} foot to the ground"
   puts "Bounce #{starting_foot} foot back up slightly, kicking it a few inches back"
-  puts "Briefly raise the opposite foot then the #{start_foot}"
+  puts "Briefly raise the opposite foot then the #{starting_foot}"
 end
 
 def bob_the_reins
